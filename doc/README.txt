85c0f419d4385a1fda3d9c6f6b4dd091bf1c21d9
Peter Sels <peter@Herettix.local>
2012-08-30 11:02:46 +0200

    added README and did some Doxygen file changes. Also added tree.pl and scripts to run it for *nix and windows

---
+++ doc/README.txt
@@ -0,0 +1,27 @@
+README.txt
+
+
+Installation Instructions
+
+This software has been built, installed and run on:
+- Apple OS X 10.8.1
+- Linux Ubuntu
+- Windows XP
+- Windows 7
+
+It is set up generically, using CMake for Makefile and/or project generation.
+
+
+Dependencies:
+- CMake (free)
+- Boost library >= v 1.50.0 (free)
+- STL (included in c++ compiler, free)
+
+Via CMake, it can setup project files for:
+- Microsoft Windows Visual Studio C++ (Download and use the CMake GUI for Windows)
+- plain Unix Makefiles, to be used at command line, typing just 'make' (just type ./redoCmx)
+- Apples XCode IDE  (just type ./redoCmx xcode)
+- Eclipse  (just type ./redoCmx eclipse)
+
+
+
---
+++ win_tree.pl
@@ -0,0 +1,135 @@
+# This script is sourced from:
+# http://sandeep-vaniya.blogspot.be/2008/04/perl-script-for-directory-tree.html
+
+$no_of_args = @ARGV;
+$expand_size = 5;
+$max_width = 10;
+
+if(($no_of_args == 0) || ($no_of_args == 2))
+{
+  print "ERROR: Insufficient argument\n";
+  goto help;
+}
+elsif($no_of_args == 3)
+{
+  if($ARGV[1] !~ "-width")
+  {
+    print "ERROR: Invalid argument\n";
+    goto help;
+  }
+  elsif(($ARGV[2] > $max_width) || ($ARGV[2] < 2))
+  {
+    print "ERROR: Width should not lie outside 2 to $max_width.\n";
+    goto help;
+  }
+  else
+  {
+    $expand_size = $ARGV[2];
+  }
+}
+$dir_path = $ARGV[0];
+if(!(-d $dir_path))
+{
+  print "ERROR: $dir_path doesn't exist\n";
+  goto help;
+}
+
+if(($dir_path =~ /^\.$/) || ($dir_path =~ /^\.\/$/) || ($dir_path =~ /^\.\\$/))
+{
+  $string = `cd`;
+  $dir_name = $string;
+  $dir_path = $string;
+}
+else
+{
+  $dir_name = "$dir_path\n";
+}
+
+@array = `dir /B /S $dir_path`;
+$array_size = @array;
+$line_count = 0;
+
+while($line_count < $array_size)
+{
+  $array[$line_count] =~ s/\\/\//g;
+  $line_count++;
+}
+
+$line_count = 0;
+
+print "+----$dir_name";
+$temp = $dir_path;
+chop($temp);
+$temp =~ s/\\/\//g;
+while($line_count < $array_size)
+{
+  if($array[$line_count] =~ /$temp\/(.*)/)
+  {
+    $array[$line_count] = "--$1\n";
+  }
+
+  $array[$line_count] =~ s/.[^\/]*\//--/g;
+  $line_count++;
+}
+
+$line_count = 0;
+while($line_count < $array_size)
+{
+  $array[$line_count] = "\|\-".$array[$line_count];
+  $string = " " x ($expand_size - 1);
+  while($array[$line_count] =~ /--/)
+  {
+    $array[$line_count] =~ s/--//;
+    $array[$line_count] =~ s/\|\-/|$string|-/;
+  }
+  $string = "-" x ($expand_size - 1);
+  $array[$line_count] =~ s/\-/$string/;
+  $array[$line_count] =~ s/\|$string/\+$string/;
+  $line_count++;
+}
+
+@cross = 0;
+$line_count = 0;
+while($line_count < $array_size)
+{
+  $line = $array[$array_size - $line_count - 1];
+  @line_array = split(//, $line);
+  $no_of_char = @line_array;
+  $char_count = 0;
+  while($char_count < $no_of_char)
+  {
+    if($line_array[$char_count] =~ /\+/)
+    {
+      $cross[$char_count] = 1;
+    }
+    elsif($line_array[$char_count] =~ /\|/)
+    {
+      if($cross[$char_count] == 0)
+      {
+        $line_array[$char_count] = " ";
+      }
+    }
+    else
+    {
+      $cross[$char_count] = 0;
+    }
+    $char_count = $char_count + $expand_size;
+  }
+  
+  $array[$array_size - $line_count - 1] = join("", @line_array);
+  $line_count++;
+}
+
+print @array;
+exit;
+
+help:
+printf("#####################################################################\n");
+printf("# To run the script,\n");
+printf("# perl $0 <PATH> -width <width_no>\n");
+printf("#    e.g. \"perl $0 . \" displays tree view of current dir\n");
+printf("# \"-width\" expand or compress the tree. Max = $max_width, Min = 2\n");
+printf("# Don't give relative path.....\n");
+printf("# Default width = 5\n");
+printf("#####################################################################\n");
+
